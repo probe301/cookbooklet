25e962b3217bfa4efc3e77298788e6f3ecee56f4
Probe <probe301@gmail.com>
2019-08-29 15:46:11 +0800

    update crawler

--- crawler_and_web_request.py
+++ crawler_and_web_request.py
@@ -15,6 +15,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import FirefoxProfile
+from selenium.webdriver.support.ui import Select
 
 from peewee import SqliteDatabase
 from peewee import CharField
@@ -34,7 +35,9 @@ from peewee import JOIN
 
 
 
-# requests download image
+
+
+# requests download image (file)
 import shutil
 def download_image(url, path):
   print(f'download img {url} ...')
@@ -44,6 +47,9 @@ def download_image(url, path):
       r.raw.decode_content = True
       shutil.copyfileobj(r.raw, f)
   print('done')
+
+
+
 
 
 
@@ -65,6 +71,8 @@ text_data = bytes.decode(resp.content)  # for text
 import json
 json_data = json.loads(bytes.decode(resp.content, encoding='utf-8'))  # for json
 json_data = resp.json()                                               # for json
+
+
 
 
 
@@ -93,6 +101,8 @@ def async_fetch_images_sample():
 
 
 
+
+
 # pyquery sample
 doc = pq(htmltext)
 for div in doc.find('#content-text > .tabbertab'):
@@ -104,3 +114,93 @@ for div in doc.find('#content-text > .tabbertab'):
 
 
 
+
+
+"""
+sample
+selenium custom driver 
+css selector
+"""
+
+# selenium custom driver
+def get_windows_firefox_driver():
+  gecko_path = '/Users/probe/git/geckodriver'
+  gecko_path = 'C:/Program Files/Mozilla Firefox/geckodriver.exe'
+  profile = webdriver.FirefoxProfile()
+  # profile.set_preference("browser.download.dir","/Users/probe/git/Crawler/");
+  # profile.setPreference("browser.download.folderList", 2);
+  profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream,text/csv");
+  profile.set_preference("browser.download.dir",os.getcwd())
+
+  # if proxies:
+  #   profile.set_preference("network.proxy.type", 1)
+  #   profile.set_preference("network.proxy.socks", "127.0.0.1")
+  #   profile.set_preference("network.proxy.socks_port", 1080)
+  #   profile.set_preference("network.proxy.socks_version", 5)
+  #   # profile.setAssumeUntrustedCertificateIssuer(False)
+  #   profile.update_preferences()
+  driver = webdriver.Firefox(firefox_profile=profile,
+                             executable_path=gecko_path)
+  return driver
+
+
+class DriverFindElementByCSS:
+  ''' usage:
+      driver / css(pat)  => one element
+      driver // css(pat)  => [element list]
+  '''
+  def __call__(self, pat):
+    self.pat = pat
+    return self
+  def __rtruediv__(self, driver):
+    return driver.find_element_by_css_selector(self.pat)
+  def __rfloordiv__(self, driver):
+    return driver.find_elements_by_css_selector(self.pat)
+css = DriverFindElementByCSS()
+
+
+def fetch_one_page(driver, state_code):
+  driver.get('https://ofmpub.epa.gov/apex/sfdw/f?p=108:200::::::')
+  print('get US map page...')
+  # state_code = '09'
+  selector = Select(driver / css('#P200_STATE'))
+  selector.select_by_value(state_code)
+  search_button = driver / css('#P200_SEARCH')
+  search_button.click()
+  print('click search button...')
+  WebDriverWait(driver, 120).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, '#SFDWIR_row_select'))
+  )
+  change_all_selector = Select(driver / css('#SFDWIR_row_select'))
+  change_all_selector.select_by_value('100000')
+  print('click show all rows...')
+  WebDriverWait(driver, 120).until(
+      EC.invisibility_of_element_located((By.CSS_SELECTOR, 'span.u-Processing-spinner'))
+  )
+  WebDriverWait(driver, 400).until(
+      EC.presence_of_element_located((By.CSS_SELECTOR, '.t-fht-tbody'))
+  )
+
+  time.sleep(10)
+  print('prepare to fetch')
+  tables_header, tables_body = driver // css('.a-IRR-table')
+  headers = [td.text.replace('\n', ' ') for td in tables_header // css('th')]
+
+
+  content = []
+  for row in PyQuery(tables_body.get_attribute('outerHTML'))('tr'):
+    row = PyQuery(row)
+    if not row:
+      continue
+    row = [td.text for td in row('td')]
+    if row and row[0]:
+      content.append(row)
+  record_csv(headers, content, 'epa_water_system_summary_{}_[{}].csv'.format(state_code, len(content)))
+
+
+state_codes = '''
+  AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY AS MP PR VI 01 02 03 04 05 06 07 08 09 10 NN
+'''.strip().split(' ')
+driver = get_windows_firefox_driver()
+for state_code in state_codes[:3]:
+  fetch_one_page(driver=driver, state_code=state_code)
