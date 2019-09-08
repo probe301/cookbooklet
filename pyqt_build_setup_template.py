d3ebdf0d335f2a62844c4f2441be05754fe989e7
Probe <probe301@gmail.com>
2019-09-08 16:48:22 +0800

    add pip usage

--- pyqt_build_setup_template.py
+++ pyqt_build_setup_template.py
@@ -1,4 +1,27 @@
 
+
+# 检查包版本
+# pip list | grep arrow
+# >>> arrow     0.14.2
+# 安装包的特定版本
+# pip install arrow==0.14.2
+
+
+# 安装 requirements.txt 中的包
+# pip install -r requirements.txt
+
+
+# 检测 项目中用到的包
+# pip install pipreqs
+# pipreqs ./ --encoding=utf8 --use-local --print
+# >>> py_gfm==0.1.4
+# >>> feedgen==0.7.0
+# >>> ...
+# >>> Jinja2==2.10.1
+# >>> pyfiglet==0.8.post1
+# >>> Flask==1.0.3
+
+
 import sys
 import os
 from cx_Freeze import setup, Executable
